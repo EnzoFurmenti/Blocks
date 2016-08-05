07fb16810efbab921785cda3a4fe8f5344df46fa
EnzoFurmenti <ducheenzo@mail.ru>
2016-08-05 10:02:18 +0300

    add level pupil

--- Blocks/AppDelegate.m
+++ Blocks/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+typedef void (^BlockNotParametre)(void);
+typedef void (^BlockWithParametre)(NSString*);
 @interface AppDelegate ()
 
 @end
@@ -16,30 +17,42 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    BlockNotParametre blockNP= ^{
+        NSLog(@"BlockNotParametre - Привет!!!");
+    };
+    
+    BlockWithParametre  blockWP = ^(NSString *string){
+        NSLog(@"BlockWithParametre - %@",string);
+    };
+    
+    blockNP();
+    blockWP(@"Hello");
+    
+    [self methodWithBlockNP:blockNP];
+    [self methodWithBlockWP:blockWP];
+    
+    [self methodWithBlockWPAndArg:blockWP
+                      greetingStr:@"Ciao"];
     // Override point for customization after application launch.
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+#pragma MARK -metods-
+- (void)methodWithBlockNP:(void(^)(void)) blockWithString{
+    blockWithString();
+    blockWithString();
+    blockWithString();
+}
+- (void)methodWithBlockWP:(void(^)(NSString *string)) blockWithString{
+    blockWithString(@"methodWithBlockWP - Привет");
+    blockWithString(@"methodWithBlockWP - Hello");
+    blockWithString(@"methodWithBlockWP - Ciao");
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)methodWithBlockWPAndArg:(void(^)(NSString *string)) blockWithString greetingStr:(NSString*)greetingStr{
+    blockWithString([NSString stringWithFormat:@"methodWithBlockWPAndArg - %@",greetingStr]);
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
